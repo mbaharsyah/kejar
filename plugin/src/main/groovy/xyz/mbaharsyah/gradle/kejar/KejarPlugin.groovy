aa1853d02a271b95826228228736e1839ce28d6e
Pram Baharsyah <mbaharsyah@PramCoBook.local>
2015-10-03 23:40:38 +0800

    Initial commit

---
+++ plugin/src/main/groovy/xyz/mbaharsyah/gradle/kejar/KejarPlugin.groovy
@@ -0,0 +1,41 @@
+package xyz.mbaharsyah.gradle.kejar
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.Copy;
+
+class KejarPlugin implements Plugin<Project> {
+    @Override
+    def void apply(Project project) {
+
+        // Only create tasks for library project
+        if (project.android.hasProperty('libraryVariants')) {
+
+            def outputDir = "${project.buildDir}/outputs/"
+
+            project.android.libraryVariants.all { variant ->
+
+                def taskName = "kejar${variant.name.capitalize()}"
+                def baseFilename = "${project.name}-${variant.baseName}"
+
+                project.task(taskName, type: Copy) {
+                    description "Generate jar file for ${variant.name} variant"
+                    group 'build'
+                    inputs.file("${outputDir}/aar/${baseFilename}.aar")
+                    outputs.file("${outputDir}/jar/${baseFilename}.jar")
+
+                    from(project.zipTree("${project.buildDir}/outputs/aar/${baseFilename}.aar")) {
+                        include 'classes.jar'
+                        rename 'classes.jar', "${baseFilename}.jar"
+                    }
+                    into "${outputDir}/jar"
+                }
+
+                def taskToFinalize = ":${project.name}:assemble${variant.name.capitalize()}"
+                project.getTasks().findByPath(taskToFinalize).finalizedBy taskName
+            }
+
+        }
+
+    }
+}
